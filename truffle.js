2f0a1d7fe56321d06efc00df763abe5cfb6d586d
Uxio Fuentefria <uxio@gnosis.pm>
2019-11-26 13:14:27 +0100

    Add Gnosis Safe dependencies

---
+++ truffle.js
@@ -0,0 +1,18 @@
+// @flow
+
+module.exports = {
+  migrations_directory: './migrations',
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', //* Match any network id
+    },
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 500,
+    },
+  },
+}
