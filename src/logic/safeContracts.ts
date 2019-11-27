2c522655b92636e794da1c41d4dab9fcddd15e95
Uxio Fuentefria <uxio@gnosis.pm>
2019-11-27 12:55:49 +0100

    Do gas estimation for module deployment

--- src/logic/safeContracts.ts
+++ src/logic/safeContracts.ts
@@ -3,32 +3,41 @@ import DailyLimitJson from '@gnosis.pm/safe-contracts/build/contracts/DailyLimit
 import GnosisSafeJson from '@gnosis.pm/safe-contracts/build/contracts/GnosisSafe.json'
 import ModuleManagerJson from '@gnosis.pm/safe-contracts/build/contracts/ModuleManager.json'
 import ProxyFactoryJson from '@gnosis.pm/safe-contracts/build/contracts/ProxyFactory.json'
-import SafeProxy from '@gnosis.pm/safe-contracts/build/contracts/Proxy.json'
+import ProxyJson from '@gnosis.pm/safe-contracts/build/contracts/Proxy.json'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 export const SENTINEL_ADDRESS = '0x0000000000000000000000000000000000000001'
 
 
-const getDailyLimitContract = (web3: any, address: string) => {
-  return new web3.eth.Contract(DailyLimitJson.abi, address)
-}
-
-const getSafeContract = (web3: any, address: string) => {
-  return new web3.eth.Contract(GnosisSafeJson.abi, address)
-}
-
-const deployDailyLimitContract = async (web3: any) => {
-  let dailyLimitContract = new web3.eth.Contract(DailyLimitJson.abi)
-  let accounts = await web3.eth.accounts
-  let defaultAccount = accounts[0]
-  let deployedContract = await dailyLimitContract.deploy({'data': DailyLimitJson.bytecode, 'arguments': [[], []]}).send({'from': defaultAccount, 'gas': 1500000})
-  return deployedContract
-}
-
 interface ModuleInfo{
     address: string
     name: string
     version: string
+}
+
+const getDefaultAccount = async (web3: any): Promise<string> => {
+  let accounts = await web3.eth.accounts
+  return accounts[0]
+}
+
+const getDailyLimitContract = (web3: any, address?: string) => {
+  return new web3.eth.Contract(DailyLimitJson.abi, address)
+}
+
+const getSafeContract = (web3: any, address?: string) => {
+  return new web3.eth.Contract(GnosisSafeJson.abi, address)
+}
+
+const deployDailyLimitContract = async (web3: any) => {
+  let dailyLimitContract = getDailyLimitContract(web3)
+  let deployData = {
+    'data': DailyLimitJson.bytecode
+  }
+  let from = await getDefaultAccount(web3)
+  let gas = await dailyLimitContract.deploy(deployData).estimateGas()
+  let deployedContract = await dailyLimitContract.deploy(deployData).send({from, gas})
+  // address is in `deployedContrat.options.address`
+  return deployedContract
 }
 
 export const getEnabledModulesForSafe = async (web3: any, address: string): Promise<ModuleInfo[]> => {
@@ -50,7 +59,7 @@ export const getEnabledModulesForSafe = async (web3: any, address: string): Prom
 
 const enableModuleForSafe = async (web3: any, address: string, moduleAddress: string) => {
   let safeContract = getSafeContract(web3, address)
-  safeContract.methods.enableModule(moduleAddress).send()
+  safeContract.methods.enableModule(moduleAddress).send('')
 }
 
 const disableModuleForSafe = async (web3: any, address: string, moduleAddress: string) => {
